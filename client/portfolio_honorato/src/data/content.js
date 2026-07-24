48605e10b922cc82ed65233e1f62fa2fdba83f1d
Wanderson <faturamento@novaterraambiental.srv.br>
2026-07-24 15:48:38 -0300

    fix: corrigir caminho para o currículo no arquivo content.js

--- client/portfolio_honorato/src/data/content.js
+++ client/portfolio_honorato/src/data/content.js
@@ -159,5 +159,5 @@ export const profile = {
   githubHandle: 'WandersonHonorato',
   linkedin: 'https://www.linkedin.com/in/wanderson-honorato/',
   linkedinHandle: 'wanderson-honorato',
-  resume: '/docs/Wanderson_Honorato_Resume.pdf',
+  resume: '/docs/Curriculo - Wanderson Honorato Araujo.pdf',
 }
